19e7a0ddb68d33b5fd7be7e5a5d2f823bb97f209
Abdu Qnauy <abduqnauy@gmail.com>
2019-11-19 03:18:40 +0200

    separating our app into client folder, added help handle backend stripe payment

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import axios from "axios";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_GB9XSYX1V22SCkQ7Hme1DEpR00sIYVGJqQ";
+
+  const onToken = token => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert("succesful payment");
+      })
+      .catch(error => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="E-Commerce SHOP++ Ltd."
+      shippingAddress
+      billingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your Total ${price}$`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
